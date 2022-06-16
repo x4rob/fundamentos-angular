92e6ab72f0dbdf36e106366b539e668936d9c9cf
Robson Ferreira <robson.s.ferreira1989@gmail.com>
2022-06-16 18:03:21 -0300

    Estudo de Fundamentos Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponenteComponent,
+    SegundoComponenteComponent
   ],
   imports: [
     BrowserModule,

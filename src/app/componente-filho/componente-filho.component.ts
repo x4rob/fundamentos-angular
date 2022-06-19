58d4c886806748d955f42468eb8c51f6f47127b0
Robson Ferreira <robson.s.ferreira1989@gmail.com>
2022-06-19 11:23:21 -0300

    finalizando interação entre componentes

--- src/app/componente-filho/componente-filho.component.ts
+++ src/app/componente-filho/componente-filho.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-componente-filho',
   templateUrl: './componente-filho.component.html',
   styleUrls: ['./componente-filho.component.css']
 })
-export class ComponenteFilhoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ComponenteFilhoComponent {
+  @Input() sobrenome = "";
+  nome = "";
+  
 }
